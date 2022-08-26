af98bb44488c848a709cbf4a632b3d02d06f9bd9
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-27 01:05:33 +0300

    install,delete,react-control..

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# weather-app
+# API="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=1d12c08f60a32d55e546fb608651ceae"
---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>hello</Button>
+    </div>
+  );
+};
+
+export default Home;
