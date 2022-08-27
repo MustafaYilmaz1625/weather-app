8e5307c36f1407cbd8ae5b78b44f60f5c75f4439
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-27 11:03:28 +0300

    store is created

---
+++ src/redux/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import weatherReducer from "../slices/weatherSlices"
+
+const store = configureStore({
+  reducer: weatherReducer,
+});
+
+export default store;
