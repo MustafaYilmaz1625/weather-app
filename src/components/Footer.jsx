16a4126f7005766ff0689a785b1b406f27b5bb63
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-11-25 01:16:34 +0300

    add updata css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,23 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 
 function Footer() {
   return (
-    <div className="fixed bottom-0 flex md:flex-row  flex-col  justify-between items-center    bg-slate-600 w-full p-3">
+    <div className="fixed bottom-0 flex flex-col md:flex-row justify-between items-center bg-slate-700 w-full p-3">
       <div className="md:flex-auto md:w-16">
-        <h1 className="text-center md:text-start pl-5">
+        <h1 className="md:text-start md:pl-5 flex justify-start text-gray-300">
           Copyright &copy; New Design 2023
         </h1>
       </div>
-      <div className="md:flex-auto md:w-64" >
-        <ul className="flex flex-row py-2 md:py-1 justify-center items-center">
-          <li className="w-[40px] h-[30px] flex  justify-between items-center rounded-xl hover:scale-150  bg-blue-600">
+      <div className="md:flex-auto md:w-16">
+        <ul className="flex flex-row py-2 md:py-1 justify-center">
+          <li className="w-[40px] h-[30px] flex  justify-between items-center rounded-xl hover:scale-150 bg-blue-400 hover:bg-blue-500">
             <a
               className="flex justify-between items-center w-full text-gray-100"
-              href="https://www.linkedin.com/in/mustafa-y%C4%B1lmaz-/"
+              href="https://www.linkedin.com/in/mustafa-y%C4%B1lmaz-4b196023a/"
             >
               <FaLinkedin size={30} />
             </a>
           </li>
-          <li className="w-[40px] h-[30px] flex justify-between items-center rounded-xl hover:scale-150  bg-[#262626]">
+          <li className="w-[40px] h-[30px] flex justify-between items-center rounded-xl hover:scale-150 bg-blue-600 hover:bg-blue-700">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="https://github.com/MustafaYilmaz1625"
@@ -29,27 +29,28 @@ function Footer() {
               <FaGithub size={30} />
             </a>
           </li>
-          <li className="w-[40px] h-[30px] flex justify-between items-center rounded-xl hover:scale-150  bg-[#5d978b]">
+          <li className="w-[40px] h-[30px] flex justify-between items-center rounded-xl hover:scale-150 bg-green-500 hover:bg-green-700">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://mail.google.com/mail/u/0/?hl=tr&tf=cm&fs=1&to=myyilmaz1625@"
-            >
-              <HiOutlineMail size={30} />
-            </a>
-          </li>
-
-          <li className="w-[40px] h-[30px] flex justify-between items-center rounded-xl hover:scale-150 bg-[#25313e]">
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.linkedin.com/in/mustafa-yılmaz-/overlay/1635494929296/single-media-viewer/"
+              className="flex justify-between items-center w-full text-gray-100"
+              href="https://www.linkedin.com/in/mustafa-y%C4%B1lmaz-/overlay/1635494929296/single-media-viewer/"
             >
               <BsFillPersonLinesFill size={30} />
             </a>
           </li>
+          <li className="w-[40px] h-[30px] flex justify-between items-center rounded-xl hover:scale-150 bg-red-600 hover:bg-red-800">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://mail.google.com/mail/u/0/?hl=tr&tf=cm&fs=1&to=mustafayilmaz1625@outlook.com"
+            >
+              <HiOutlineMail size={30} />
+            </a>
+          </li>
         </ul>
       </div>
-      <div className="md:flex-auto md:w-16">
-        <h1 className="text-center md:text-end pr-5">Privacy Policy</h1>
+      <div className="md:flex-auto md:w-0 lg:w-14">
+        <h1 className="text-center md:text-end md:pr-5 text-gray-300">
+          Privacy Policy
+        </h1>
       </div>
     </div>
   );
